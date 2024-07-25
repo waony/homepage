eeb27ad0d4fde36308d7b50953070422ae56982f
waony <166287046+waony@users.noreply.github.com>
2024-07-25 13:05:25 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var sidebarToggle = document.getElementById("sidebar-toggle");
-    var sidebar = document.getElementById("sidebar");
+const sidebarToggle = document.getElementById('sidebar-toggle');
+        const sidebar = document.getElementById('sidebar');
 
-    sidebarToggle.addEventListener("click", function() {
-        if (sidebar.classList.contains("sidebar-visible")) {
-            sidebar.classList.remove("sidebar-visible");
-        } else {
-            sidebar.classList.add("sidebar-visible");
-        }
-    });
-});
+        sidebarToggle.addEventListener('click', () => {
+            sidebar.classList.toggle('sidebar-visible');
+        });
